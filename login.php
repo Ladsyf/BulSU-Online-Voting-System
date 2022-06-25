9a87e860cc294549e10ff8a7b09bd9983662e5e6
almar <almarjavier24@gmail.com>
2022-06-25 14:52:20 +0800

    login

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+        <link rel="stylesheet" href="css/loginregister.css">
+    </head>
+<body>
+    <div class = "">
+        BulSU E-Voting System
+    </div>
+
+    <div class = "logincontainer">
+    <form>
+        <input type = "text" placeholder = "Username">
+        <input type = "password" placeholder = "Password">
+        <input type = "checkbox">
+        <input type = "submit" value = "SIGN IN">
+        <p> Forgot Password? Click <a href = "#">here</a></p>
+        <hr> <p> OR  </p><hr>
+    </form>
+
+    <button>REGISTER</button>
+    <p> Doesn't have an account? </p>
+    </div>
+</body>
+</html>
