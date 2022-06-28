21726c6eb4eccf510618908ae5bb85ef5422d968
almar <almarjavier24@gmail.com>
2022-06-28 16:01:33 +0800

    index header progress

---
+++ navigationheader.php
@@ -0,0 +1,13 @@
+
+<ul>
+  <li>
+    <img src = "img/icons/hamburger.png" class = "hamburger"/>
+  </li>
+  <li style="float:right">   
+          <div class = "ppicture" style = "background-image: url('img/bulsu.jpg')">
+          </div>
+          <h2 class = "lastname">DELA CRUZ,</h2>
+          <p class = "firstname">Juan D.</p>
+          <img src = "img/icons/login-filled.png" />
+  </li>
+</ul>
