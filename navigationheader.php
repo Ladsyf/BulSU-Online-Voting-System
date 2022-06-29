04a56a96a680834b0a71f5196265a546aad034de
almar <almarjavier24@gmail.com>
2022-06-29 14:45:18 +0800

    improved header not vertically aligned

--- navigationheader.php
+++ navigationheader.php
@@ -6,8 +6,8 @@
   <li style="float:right">   
           <div class = "ppicture" style = "background-image: url('img/bulsu.jpg')">
           </div>
-          <h2 class = "lastname">DELA CRUZ,</h2>
+          <p class = "lastname">DELA CRUZ,</p>
           <p class = "firstname">Juan D.</p>
-          <img src = "img/icons/login-filled.png" />
+          <img src = "img/icons/login-filled.png"  />
   </li>
 </ul>
