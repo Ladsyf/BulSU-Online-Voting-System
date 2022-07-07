99bbf1c9f53e89df4c5b5bc6304a5dffcef37fe9
almar <almarjavier24@gmail.com>
2022-07-07 14:19:22 +0800

    adding html on hamburger menu

--- navigationheader.php
+++ navigationheader.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="css/header.css">
 
 <ul>
   <li class = "hamburger">
@@ -11,3 +12,75 @@
           <img src = "img/icons/login-filled.png" class = "logoutBTN" />
   </li>
 </ul>
+
+<div class="navbar1">
+    <div class = "acontainer1">
+      <a href="#"><img src = "img/icons/HomeICON.png" class = "navlogo"> Home</a>
+    </div>
+    <div class = "acontainer1">
+      <a href="#"><img src = "img/icons/ElectionICON.png" class = "navlogo"> Elections</a>
+    </div>
+    <div class = "acontainer1">
+      <a href="#"><img src = "img/icons/VideoICON.png" class = "navlogo"> Campaigns</a>
+    </div>
+    <div class = "acontainer1">
+      <a href="#"><img src = "img/icons/ProfileICON.png" class = "navlogo"> Profile</a>
+    </div>
+</div>
+
+<style>
+    .navbar1{
+      display: none;
+      box-shadow: 0px -25px 40px rgba(0, 0, 0, 0.158);
+    }
+    .acontainer1{
+      display: flex;
+      justify-content: center;
+      background-color: blue;
+    }
+    .navlogo1{
+      height: 100%;
+    }
+.navbar1 {
+      background-color: white;
+      overflow: hidden;
+      position: fixed;
+      display:grid;
+      grid-template-rows: auto auto auto auto;
+      color: black;
+      background-color: red;
+      z-index: 2;
+}
+
+/* Style the links inside the navigation bar */
+.navbar1 a {
+  float: left;
+  text-align: center;
+  padding: 13px;
+  text-decoration: none;
+  color: black;
+  border-radius: 10px;
+  margin: 10px 0;
+  height: 25px;
+  width: 100%;
+  text-align: left;
+  background-color: green;
+}
+
+/* Change the color of links on hover */
+.navbar1 a:hover {
+  background-color: #ddd;
+  
+}
+
+/* Add a color to the active/current link */
+.navbar1 a.active {
+  color: black;
+  color: white;
+}
+@media only screen and (max-width: 600px) {
+      .navbar1{
+        display: none;
+      }
+    }
+</style>
